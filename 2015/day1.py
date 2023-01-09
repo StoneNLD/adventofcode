7382c0f93cd01fd707380497f46e24ca55fe5f69
Stijn Nass <stijn.nass@inspark.nl>
2023-01-09 16:35:28 +0100

    completed day1

---
+++ 2015/day1.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+import requests
+
+sessionid = input("sessionid from cookie: ")
+cookie = {'session': sessionid}
+puzzle_input = requests.get("https://adventofcode.com/2015/day/1/input", cookies=cookie)
+
+floor = 0
+position= 0
+
+for move in puzzle_input.text:
+    if floor < 0:
+        break
+    if move == "(":
+         print("going up 1 floor")
+         floor += 1
+         print("we are now on floor", floor )
+    if move == ")":
+        print("going down 1 floor")
+        floor -= 1
+        print("we are now on floor", floor )
+    position += 1
+        
+print(floor)
+print("position we entered basement: ", position)
+
+
+
+
+
