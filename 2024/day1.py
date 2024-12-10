39dff2372ecdd1ebdadefe73fcd6ab69bf97025e
Stijn Nass <stijn.nass@inspark.nl>
2024-12-10 20:43:35 +0100

    It Compiles! 50 Points For Gryffindor.

--- 2024/day1.py
+++ 2024/day1.py
@@ -3,7 +3,8 @@
 import requests
 
 def grab_input():
-    sessionid = input("sessionid from cookie: ")
+    # sessionid = input("sessionid from cookie: ")
+    sessionid = '53616c7465645f5f16478a7c79a602bcfd37c7ccd166fecaeb784d9b005613eb0d0606c7542371ed6d85c2c15050fc95f10ea99e0c428c400ee93e3b7a887e94'
     cookie = {'session': sessionid}
     response = requests.get("https://adventofcode.com/2024/day/1/input", cookies=cookie)
     return response.text.strip().split("\n")
@@ -19,9 +20,10 @@ def calculate_total_distance(distance_list):
 def test_functions():
     testlist1 = [3, 4, 2, 1, 3, 3]
     testlist2 = [4, 3, 5, 3, 9, 3]
+    test_input = [f"{a} {b}" for a, b in zip(testlist1, testlist2)]
     test_result = [2, 1, 0, 1, 2, 5]
     assert calculate_diff(testlist1, testlist2) == test_result
-    assert simiulary_score(inputlist) == 31 
+    assert simiulary_score(test_input) == 31 
 
 def part1(inputlist):
     list1 = [int(line.split()[0]) for line in inputlist]
@@ -40,10 +42,10 @@ def simiulary_score(inputlist):
     return score
 
 def part2(inputlist):
-    pass
+    print(simiulary_score(inputlist))
 
 if __name__ == "__main__":
-    test_functions()
     inputlist = grab_input()
+    test_functions()
     part1(inputlist)
-    simiulary_score(inputlist)
+    part2(inputlist)
